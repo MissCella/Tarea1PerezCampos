d1dee8ab7f63308078e9be41f6915c4cdc4f2567
Francella Campos <56238114+MissCella@users.noreply.github.com>
2021-08-11 22:47:34 -0600

    Errores arreglados de flake 8

---
+++ Flake8_corrected.py
@@ -0,0 +1,15 @@
+# Elaborado por: Francella Campos Alfaro y Gonzalo Pérez Solis
+# Fecha de Creación: 11/08/2021 22:00
+# Fecha de última Modificación: 11/08/2021 23:06
+# Versión: 3.9.6
+#####################################################################
+
+
+# Error 1: (E265) Debe existir un espacio entre el # y el texto
+# Comment
+
+# Error 2: (E211) No debe existir espacio entre el print y el '('
+print("Buenos días profesor, espero que esté teniendo un buen día :)")
+
+# Error 3: (E225) Debe existir espacio entre los operadores
+a = 2 + 5
